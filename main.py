62d7ea18e6814ca0d6dfb9f6c4fad13faf3de54c
Matsura Kota <s24c1110qm@s.chibakoudai.jp>
2025-11-30 18:07:46 +0900

    Add a command

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 import sys
 import re
+from datetime import datetime
 
 def main():
-    pattern = re.compile(r'\d{10}')
+    pattern = re.compile(r'\b(1[6-7]\d{8})(\.\d+)?\b')
 
     for line in sys.stdin:
         match = pattern.search(line)
 
         if match:
-            print(f"Found: {match.group()}")
+            ts_str = match.group()
+            ts = float(ts_str)
+            dt = datetime.fromtimestamp(ts)
+            print(dt)
 
 if __name__ == "__main__":
     main()
